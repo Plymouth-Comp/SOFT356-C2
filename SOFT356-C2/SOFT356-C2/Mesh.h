7595624e9294626f63882b322a6c6dee50520795
Connor Priest <Kon_Priest@Hotmail.com>
2020-01-19 17:40:40 +0000

    Added Model for assimp

--- SOFT356-C2/SOFT356-C2/Mesh.h
+++ SOFT356-C2/SOFT356-C2/Mesh.h
@@ -17,11 +17,14 @@ struct Vertex {
 	glm::vec3 Position;
 	glm::vec3 Normal;
 	glm::vec2 TexCoords;
+	glm::vec3 Tangent;
+	glm::vec3 Bitangent;
 };
 
 struct Texture {
 	unsigned int id;
 	std::string type;
+	std::string path;
 };
 
 
